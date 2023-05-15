64f51981c0aaae315c581483d2983c3ea69f07b6
Emad hanna <emadhanna565@gmail.com>
2023-05-15 13:41:25 +0300

    create sebha tab make counter for sebha

--- lib/ui/home/sebha/sebha_tabs.dart
+++ lib/ui/home/sebha/sebha_tabs.dart
@@ -1,12 +1,68 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class SebhaScreen extends StatelessWidget {
-  const SebhaScreen({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:islamy/const.dart';
+
+class SebhaScreen extends StatefulWidget {
+  SebhaScreen({Key? key}) : super(key: key);
 
   @override
+  State<SebhaScreen> createState() => _SebhaScreenState();
+}
+
+class _SebhaScreenState extends State<SebhaScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Sebha')),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/sebha_image.png'),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('عدد التسبيحات'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(15)),
+            alignment: Alignment.center,
+            height: 70,
+            width: 50,
+            child: Text(
+              '$counter',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () {
+              incrementCounter();
+            },
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              child: Text('سبحان الله'),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void incrementCounter() {
+    counter++;
+    setState(() {});
   }
 }
